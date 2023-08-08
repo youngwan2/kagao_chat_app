47bbfae38596339806e0352cac0a583226e894f0
KYW <qodna25@gmail.com>
2023-08-08 22:53:04 +0900

    초기셋팅 커밋

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ConnectionState from './components/ConnectionState.vue'
+</script>
+
+<template>
+  <header>
+    <h1>title</h1>
+    <ConnectionState/>
+    
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/ConnectionState.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        연결상태:{{connected}}
+    </div>
+</template>
+
+<script lang="ts">
+import {state} from '../socket'
+import {computed} from 'vue'
+
+export default {
+    setup() {
+        const connected = computed(()=> {return state.connected})
+
+        return {connected}
+        }
+    }
+
+</script>
