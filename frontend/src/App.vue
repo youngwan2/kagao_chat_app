f89342cb3fc7a18dff11913628994d1be3d545e7
KYW <qodna25@gmail.com>
2023-08-10 21:42:44 +0900

    채팅창 ui 디자인 작업 및 컴포넌트 분리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,124 @@
-
 <template>
-  <header>
-    <h1>title</h1>
-    <ConnectionState />
-    <form @submit.prevent>
-      <input type="text" @keyup.enter="sendMessage" />
-    </form>
-  </header>
+  <div class="container">
+    <!-- 좌측 유저 목록 및 검색 영역 -->
+    <Aside></Aside>
+
+    <!-- 우측 헤더 영역 -->
+    <section class="right-container">
+      <Header></Header>
+
+      <!-- 우측 하단 메인의 대화창 영역 -->
+      <section class="main">
+        <ul class="content">
+          <li><span>유저가 말함</span> <span>보낸시간</span></li>
+          <li><span>내가 말함</span> <span>보낸시간</span></li>
+        </ul>
+      </section>
+      <!-- 유저 메시지 입력창 폼 -->
+      <form @submit.prevent class="user_input_form">
+        <input type="text" @keyup.enter="sendMessage" :v-model="message.userInput" />
+        <button class="submit_btn" @click="sendMessage">
+          <svg
+            style="fill: white"
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 -100 550 580"
+          >
+            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M11.1 260.2c-21.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"
+            />
+          </svg>
+        </button>
+      </form>
+    </section>
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { io } from 'socket.io-client'
-export default {
-  setup() {
-    const socket = io('http://localhost:3000')
+import { reactive } from 'vue'
+import Aside from './components/Aside.vue'
+import Header from './components/Header.vue'
 
-    function sendMessage() {
-      console.log('메시지를 보냈다.')
+const socket = io('http://localhost:3000')
 
-      socket.emit('send',"전달되는지 확인")
-    }
+const message = reactive({
+  messages: [],
+  userInput: ''
+})
 
-    return {
-      sendMessage
-    }
-  }
+function sendMessage() {
+  console.log('메시지를 보냈다.')
+  socket.emit('send', '전달되는지 확인')
 }
 </script>
 <style scoped>
+.container {
+  display: flex;
+  height: 100vh;
+}
+
+li {
+  list-style: none;
+}
+.right-container {
+  margin: 10px auto;
+  color: white;
+  text-align: center;
+  display: flex;
+  width: 66%;
+  flex-direction: column;
+  height: 100vh;
+}
+
+/* 메인(대화창) */
+.main {
+  border-radius: 20px;
+  margin: 10px 0;
+  height: 100%;
+}
+
+/* form ui */
+.user_input_form {
+  box-shadow: 2px 2px 5px 1px rgb(5, 5, 5);
+  background: #303346;
+  border-radius: 20px;
+  padding: 20px;
+  position: fixed;
+  bottom: 9px;
+  width: 66%;
+}
+
+.user_input_form input {
+  padding: 20px;
+  width: 75%;
+  transition: 0.5s;
+  border-top-left-radius: 10px;
+  border: none;
+  border-bottom-left-radius: 10px;
+}
+
+.user_input_form input:focus {
+  outline: none;
+  box-shadow: inset 0 0 3px 1px rgb(9, 45, 162);
+}
+
+/* 전송버튼 */
+.user_input_form button {
+  background: #6785ff;
+  box-shadow: inset 0 0 0 0;
+  font-size: 18px;
+  border: none;
+  border-bottom-right-radius: 10px;
+  border-top-right-radius: 10px;
+  margin: 0 10px;
+  transition: 0.5s;
+  padding: 13.5px 15px 17.5px 15px;
+}
+
+.user_input_form button:hover {
+  box-shadow: inset 300px -300px 5px 2px rgb(49, 96, 213);
+  cursor: pointer;
+}
 </style>
---
+++ frontend/src/components/Aside.vue
@@ -0,0 +1,225 @@
+<template>
+  <div class="outer_container">
+    <aside class="aside" :class="display">
+      <div style="width: 100%; height: 100vh">
+        <div class="aside_search">
+          <label for="user_search"
+            ><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+              <path
+                d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+              /></svg
+          ></label>
+          <input type="text" id="user_search" />
+        </div>
+        <ul class="aside_userlist">
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+          <li>
+            <img src="" alt="프로필" class="profile" width="50" height="50" />
+            <div>
+              <p>유저이름</p>
+              <p>상태메시지</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <button class="display" @click="offDisplay">{{ '<<' }}</button>
+    </aside>
+    <button v-show="!displayState.display" class="display outer_display" @click="onDisplay">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    const displayState = reactive({
+      display: true
+    })
+
+    let display = ref('on')
+
+    function offDisplay() {
+      display.value = 'off'
+      displayState.display = !displayState.display
+    }
+
+    function onDisplay() {
+      display.value = 'on'
+      displayState.display = !displayState.display
+    }
+
+    return {
+      offDisplay,
+      onDisplay,
+      displayState,
+      display
+    }
+  }
+}
+</script>
+<style scoped>
+.outer_container {
+  opacity: 0.9;
+
+  position: fixed;
+  z-index: 1000;
+  min-width: 30%;
+  color: white;
+  max-width: 30%;
+  width: 100%;
+  margin: 10px 10px 0 10px;
+}
+li {
+  list-style: none;
+}
+.aside {
+  box-shadow: 2px 2px 5px 1px rgb(5, 5, 5);
+  position: relative;
+  color: white;
+  justify-content: space-around;
+  display: flex;
+  background: #303346;
+  width: 100%;
+  border-radius: 20px;
+  transition: 1s;
+  overflow: auto scroll;
+}
+
+.aside.on {
+  transform: translate(0);
+  visibility: visible;
+  opacity: 1;
+}
+
+.aside.off {
+  transform: translate(-800px);
+  visibility: hidden;
+  opacity: 0;
+}
+
+.aside::-webkit-scrollbar {
+  width: 5px;
+}
+/* 검색영역 */
+.aside_search {
+  margin: 10px auto;
+  width: 90%;
+  border-radius: 20px;
+  background: white;
+  text-align: center;
+}
+
+/* 돋보기 역할 */
+.aside_search label {
+  position: relative;
+  color: black;
+  bottom: -3px;
+  left: -3%;
+}
+.aside_search input {
+  padding: 15px;
+  border: none;
+  background: transparent;
+  border-radius: 10px;
+  width: 80%;
+}
+
+.aside_search input:focus {
+  outline: none;
+}
+
+/* 유저리스트 영역 */
+.aside_userlist {
+  padding-left: 0;
+  margin-top: 3rem;
+}
+
+.aside_userlist li {
+  margin: 10px 13px;
+  padding: 20px 0;
+  border-radius: 15px;
+  background: #3a3e56;
+  display: flex;
+}
+
+.aside_userlist li img {
+  border-radius: 50%;
+  background: black;
+  margin: 5px;
+}
+
+.aside_userlist li div {
+  margin-left: 5px;
+}
+
+/* 디스플레이 컨트롤 버튼 */
+.aside .display {
+  cursor: pointer;
+  border: none;
+  transition: 0.5s;
+}
+
+.aside .display:hover {
+  box-shadow: 0 0 5px 2px white;
+  font-size: 16px;
+}
+
+.outer_display {
+  border: none;
+  transition: 0.5s;
+  position: fixed;
+  top: 0;
+  height: 100vh;
+  left: 0;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
+}
+
+.outer_display:hover {
+  font-size: 19px;
+  cursor: pointer;
+  box-shadow: 0 0 5px 2px white;
+}
+</style>
